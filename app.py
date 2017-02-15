6d4742113df4b18f11d5abfa309521ba31c460c4
badgrenola <mattbrealeyis@gmail.com>
2017-02-15 12:38:20 +0000

    Update to allow for versions without an Entity attached (e.g. concepts)

--- app.py
+++ app.py
@@ -81,14 +81,14 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
             return
         else : 
             recipient = result['sg_recipient']
-            self.log_info("\nRecipient : %s\n" % recipient)
+            self.log_info("\nRecipient : %s" % recipient)
 
         # self.log_info(shotgun.schema_field_read('Version'))
         # self.log_info(context.entity)
 
         #Find all Versions where playlist matches the ID and the project matches
         filters = [['playlist', 'is', {'type':'Playlist', 'id':playlistID}]]
-        fields = ['playlist.Playlist.code', 'sg_sort_order', 'version', 'version.Version.code', 'version.Version.user', 'version.Version.entity', 'version.Version.sg_path_to_movie']
+        fields = ['playlist.Playlist.code', 'sg_sort_order', 'version', 'version.Version.code', 'version.Version.user', 'version.Version.entity', 'version.Version.sg_path_to_movie', 'version.Version.sg_path_to_frames']
         order=[{'column':'sg_sort_order','direction':'asc'}]
         versionConnections = shotgun.find('PlaylistVersionConnection', filters, fields, order)
 
@@ -103,11 +103,13 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         for versionConnection in versionConnections:
             version = versionConnection['version']
             pathToMovie = versionConnection['version.Version.sg_path_to_movie']
+            pathToFrames = versionConnection['version.Version.sg_path_to_frames']
 
-            if pathToMovie :
-                versionConnectionsToProcess.append(versionConnection)
-            else : 
+            #Find versions with paths
+            if pathToMovie == None and pathToFrames == None : 
                 versionConnectionsWithoutPath.append(versionConnection)
+            else : 
+                versionConnectionsToProcess.append(versionConnection)
 
             #Set playlist name
             if not playlistName :
@@ -121,41 +123,35 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         #Report sunmmary
         self.log_info(" ")
         self.log_info("For playlist '%s' :" % playlistName)
-        self.log_info("    There are %s versions with a filename present." % len(versionConnectionsToProcess))
-        self.log_info("    There are %s versions WITHOUT a filename present." % len(versionConnectionsWithoutPath))
-        self.log_info(" ")
+        self.log_info("....There are %s versions with a filename present." % len(versionConnectionsToProcess))
+        self.log_info("....There are %s versions WITHOUT a filename present.\n" % len(versionConnectionsWithoutPath))
 
         #Get path to copy to
         projectPath = tank.project_path
         dailiesDir = os.path.join(projectPath, 'clientIO', 'outgoing')
         playlistDir = os.path.join(dailiesDir, playlistName)
 
-        #Log preview state
-        if preview :
-            self.log_info("In Preview Mode")
-            self.log_info(" ")
-        else : 
-            self.log_info("Not in Preview Mode")
-            self.log_info(" ")
+
+        self.log_info("Making Directory :")
 
         #Check path exists
         if not os.path.exists(dailiesDir):
-            self.log_warning('Dailies directory cannot be found : %s' % dailiesDir)
+            self.log_warning('....clientIO/outgoing directory cannot be found.')
             return
 
         #Check if playlist dir exists
         if os.path.exists(playlistDir):
-            self.log_info("Playlist directory already exists : %s" % playlistDir)
+            self.log_info("....Playlist directory already exists")
         else : 
-            self.log_info("Playlist directory doesn't exist. Making directory : %s" % playlistDir)
+            self.log_info("....Playlist directory doesn't exist. Making directory...")
             if not preview:
                 try :
                     os.makedirs(playlistDir)
                 except Exception as e :
-                    self.log_warning("Could not make playlist directory. Aborting. Error : %s" % e)
+                    self.log_warning("....Could not make playlist directory. Aborting. Error : %s" % e)
                     return
             else : 
-                self.log_info("PREVIEW MODE : Not creating directory.")
+                self.log_info("....PREVIEW MODE : Not creating directory.")
 
         #Store created/existing files
         createdFiles = []
@@ -163,59 +159,74 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         failed = []
 
         #Loop through versions to process
-        self.log_info(" ")
         for versionConnection in versionConnectionsToProcess:
             version = versionConnection['version']
             user = versionConnection['version.Version.user']
             entity = versionConnection['version.Version.entity']
             pathToMovie = versionConnection['version.Version.sg_path_to_movie']
+            pathToFrames = versionConnection['version.Version.sg_path_to_frames']
 
-            #Report individual version
-            self.log_info("Processing '%s' (Asset:'%s') by %s" % (version['name'], entity['name'], user['name']))
+            #Check for an entity connection
+            if entity == None : 
+                entityConnection = False
+                self.log_info("\nProcessing '%s' by %s" % (version['name'], user['name']))
+            else : 
+                entityConnection = True
+                self.log_info("\nProcessing '%s' (Asset:'%s') by %s" % (version['name'], entity['name'], user['name']))
+
+
+            #Check whether we need to use the path_to_movie or path_to_frames
+            sourcePath = None
+            if pathToMovie == None : 
+                if pathToFrames == None : 
+                    self.log_info("....There are no files associated with this version. Skipping.")
+                    continue
+                else :
+                    sourcePath = pathToFrames
+            else : 
+                sourcePath = pathToMovie 
 
             #Check the file still exists
-            if os.path.exists(pathToMovie):
-                self.log_info("Source file found on disk")
-            else : 
-                self.log_info("Source file NOT found on disk. Skipping.")
-                failed.append(pathToMovie)
+            if not os.path.exists(sourcePath):
+                self.log_info("....Source file NOT found on disk.")
+                failed.append(sourcePath)
 
                 #Attempt to see if it's a user problem
-                if '/Users/' in pathToMovie : 
-                    self.log_info('Source file appears to be on a users machine.')
+                if '/Users/' in sourcePath : 
+                    self.log_info("....(It appears to be on a users machine)")
 
                 continue
 
             #Do the copy if it doesn't already exist
-            fileName = os.path.split(pathToMovie)[1]
+            fileName = os.path.split(sourcePath)[1]
             destinationFilePath = os.path.join(playlistDir, fileName)
 
             if os.path.exists(destinationFilePath):
-                self.log_info("File already exists for this Version. Skipping the copy.")
+                self.log_info("....Destination file already exists for this Version.")
                 existingFiles.append(destinationFilePath)
             else : 
-                self.log_info("Creating a copy : %s >> %s" % (pathToMovie, destinationFilePath))
+                self.log_info("....Copying file...")
                 if not preview :
                     try : 
                         #Do the copy
-                        # cmd = "ln -s %s %s" % (pathToMovie,destinationFilePath)
+                        # cmd = "ln -s %s %s" % (sourcePath,destinationFilePath)
                         # os.system(cmd)
-                        copy(pathToMovie, destinationFilePath)
-                        self.log_info("File successfully copied.")
+                        copy(sourcePath, destinationFilePath)
+                        self.log_info("....File successfully copied.")
                         createdFiles.append(destinationFilePath)
                     except Exception as e :
-                        self.log_warning("Could not copy file. Skipping file. Error : %s" % e)
-                        failed.append(pathToMovie)
+                        self.log_warning("....Could not copy file. Skipping file. Error : %s" % e)
+                        failed.append(sourcePath)
                         continue
                 else : 
-                    self.log_info("PREVIEW MODE : Not copying file.")
+                    self.log_info("....PREVIEW MODE : Not copying file.")
                     createdFiles.append(destinationFilePath)
 
             #Special case PSD convert - currently OSX only
             if sys.platform == 'darwin' :
                 #If the destination file is a PSD, we can convert it to PSD on the command line
                 if os.path.splitext(destinationFilePath)[1] in ['.psd', '.PSD'] :
-                    self.log_info( "File is a PSD. Attempting conversion on the command line...")
+                    self.log_info( "....File is a PSD. Attempting conversion on the command line...")
 
                     #Make the PNG File path
                     pngFilePath = os.path.join( os.path.dirname(destinationFilePath), "%s.png" % os.path.splitext(destinationFilePath)[0] )
@@ -224,14 +235,19 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
                             cmd = "sips -s format png %s --out %s" % (destinationFilePath, pngFilePath)
                             process = subprocess.Popen(cmd.split())
                             process.wait()
-                            self.log_info( "File conversion completed.")
+                            self.log_info( "....File conversion completed.")
                         else : 
-                            self.log_info( "PREVIEW MODE : Not converting file.")
+                            self.log_info( "....PREVIEW MODE : Not converting file.")
                     else : 
-                        self.log_info( "PNG version already exists")
+                        self.log_info( "....PNG version already exists")
+
+            #We only update the version info if there is an entity present on the version
+            if not entityConnection:
+                self.log_info("....Version has no entity connection. Skipping the version info update...")
+                continue
 
             #Get the versionNumber, sendToValue and sendDate
-            self.log_info( "Updating the version info...")
+            self.log_info( "....Updating the version info...")
             versionNumber = self.returnVersionNumberIntFromStringOrNone(fileName)
             versionNumberString = 'v%s' % str(versionNumber).zfill(4)
             today = datetime.date.today()
@@ -262,7 +278,7 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
                         previousVersionSentInt = intEnt_versionNumber
                         previousVersionSent = ent_versionNumber
 
-            self.log_info("Info to update : %s, %s, %s, %s" % (versionNumberString, recipient, sendDate, previousVersionSent))
+            self.log_info("....Info to update : %s, %s, %s, %s" % (versionNumberString, recipient, sendDate, previousVersionSent))
 
             #Update the version with the sent to, send date, and version number info
             updatedData = {
@@ -274,18 +290,16 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
 
             if not preview :
                 result = shotgun.update('Version', version['id'], updatedData)
-                self.log_info("Version information updated.")
-                self.log_info(result)
+                self.log_info("....Version information updated.")
             else : 
-                self.log_info("PREVIEW MODE : Not actually writing Version info")
+                self.log_info("....PREVIEW MODE : Not actually writing Version info")
 
             self.log_info(" ")
 
 
         #Report
-        self.log_info("Created %s files" % len(createdFiles))
-        self.log_info("Found %s existing files" % len(existingFiles))
-        self.log_info("%s files failed" % len(failed))
-
-        self.log_info("Finished")
+        self.log_info("\nFinished")
+        self.log_info("....Created %s files" % len(createdFiles))
+        self.log_info("....Found %s existing files" % len(existingFiles))
+        self.log_info("....%s files failed" % len(failed))
 
