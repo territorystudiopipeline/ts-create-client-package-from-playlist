da7b8b2b9ef38434e264cc631cb8366cf2912610
Sean Feeley <sean_feeley@MacBook-Pro.local>
2017-09-27 18:15:39 +0100

    work in progress

--- app.py
+++ app.py
@@ -13,9 +13,8 @@ App that copies all of the versions in the chosen playlist to a dated folder in 
 """
 
 import tank
-import sys
 import os
-import subprocess
+import subprocess as sub
 import re
 import glob
 import traceback
@@ -23,21 +22,34 @@ import datetime
 from shutil import copy
 
 
+video_exts = [".3g2", ".3gp", ".amv", ".avi", ".f4a", ".f4b", ".f4p", ".f4v", ".flv",
+              ".flv", ".m2v", ".m4p", ".mkv", ".mov", ".mp2", ".mp4", ".mpe", ".mpeg",
+              ".mpg", ".mpv", ".ogg", ".ogv", ".qt", ".rm", ".svi", ".vob", ".wmv", ".yuv"]
+
+nuke_exts = [".nk"]
+
+
 class CopyPlaylistVersionsToFolder(tank.platform.Application):
 
-    all_files = set()
-    copied = set()
-    not_copied = set()
-    missing = set()
-    already_existing = set()
+    all_movs = set()
+    movs_copied = set()
+    movs_not_copied = set()
+    movs_missing = set()
+    movs_already_existing = set()
+
+    all_nks = set()
+    nks_copied = set()
+    nks_not_copied = set()
+    nks_missing = set()
+    nks_already_existing = set()
+    nks_submitted_for_export = set()
 
     def init_app(self):
-        entity_types = self.get_setting("entity_types")
         deny_permissions = self.get_setting("deny_permissions")
         deny_platforms = self.get_setting("deny_platforms")
 
         p = {
-            "title": "(Testing) Copy Playlist Files to Folder",
+            "title": "(Testing) Create Client package",
 
             "deny_permissions": deny_permissions,
             "deny_platforms": deny_platforms,
@@ -45,10 +57,10 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         }
 
         self.engine.register_command(
-            "copyPlaylistVersionsToFolder", self.copyPlaylistVersionsToFolder, p)
+            "createClientPackage", self.create_client_package, p)
 
         p = {
-            "title": "(Testing) Preview Copy Playlist Files to Folder",
+            "title": "(Testing) Preview Create Client package",
 
             "deny_permissions": deny_permissions,
             "deny_platforms": deny_platforms,
@@ -56,7 +68,7 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         }
 
         self.engine.register_command(
-            "copyPlaylistVersionsToFolder_preview", self.copyPlaylistVersionsToFolder_preview, p)
+            "previewCreateClientPackage", self.create_client_package_preview, p)
 
     def returnVersionNumberIntFromStringOrNone(self, fileString):
         regex = "_[vV]\d+"
@@ -67,25 +79,33 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         intVersion = int(versionStringMatch[2:])
         return intVersion
 
-    def copyPlaylistVersionsToFolder_preview(self, entity_type, entity_ids):
-        self.copyPlaylistVersionsToFolder(
+    def create_client_package_preview(self, entity_type, entity_ids):
+        self.create_client_package(
             entity_type, entity_ids, preview=True)
 
-    def copyPlaylistVersionsToFolder(self, entity_type, entity_ids, preview=False):
+    def create_client_package(self, entity_type, entity_ids, preview=False):
         try:
             playlist = self.get_playlist(entity_type, entity_ids)
             versions = self.get_playlist_versions(playlist)
-            published_files = self.get_published_files(versions)
+            published_files = self.get_published_files(playlist, versions)
             self.copy_files_to_playlist_location(playlist, published_files, preview)
             self.log_info("")
             if not preview:
                 self.update_version_info(playlist, versions)
             self.log_info("")
             self.log_info("Finished")
-            self.log_info("Total files found: %d" % len(self.all_files))
-            self.log_info("Files missing: %d" % len(self.missing))
-            self.log_info("Files copied: %d" % len(self.copied))
-            self.log_info("Files already existing: %d" % len(self.already_existing))
+            self.log_info("--------")
+            self.log_info("Quicktimes chosen: %d" % len(self.all_movs))
+            self.log_info("Quicktimes missing: %d" % len(self.movs_missing))
+            self.log_info("Quicktimes copied: %d" % len(self.movs_copied))
+            self.log_info("Quicktimes already existing: %d" % len(self.movs_already_existing))
+            self.log_info("")
+            self.log_info("Scripts chosen: %d" % len(self.all_nks))
+            self.log_info("Scripts missing: %d" % len(self.nks_missing))
+            self.log_info("Scripts copied: %d" % len(self.nks_copied))
+            self.log_info("Scripts already existing: %d" % len(self.nks_already_existing))
+            self.log_info("Deadline jobs sent for export: %d" % len(self.nks_submitted_for_export))
+
             if preview:
                 self.log_info("PREVIEW MODE, no files copied.")
 
@@ -107,7 +127,7 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         context = self.tank.context_from_entity(entity_type, entity_ids[0])
         playlistID = context.entity['id']
         result = self.tank.shotgun.find_one(
-            "Playlist", [['id', 'is', playlistID]], ['sg_recipient', 'code'])
+            "Playlist", [['id', 'is', playlistID]], ['sg_published_files', 'sg_recipient', 'code'])
         if not result.get('sg_recipient'):
             raise Exception("Playlist has no recipient")
         return result
@@ -126,29 +146,112 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
             versions.append(connection['version'])
         return versions
 
-    def get_published_files(self, versions):
+    def get_published_files(self, playlist, versions):
         published_files = []
         for version in versions:
             published_files += self.tank.shotgun.find("PublishedFile",
                                                       [['version.Version.id', 'is', version['id']]],
                                                       ['path',
                                                        'sg_publish_path',
-                                                       'code'])
+                                                       'code',
+                                                       'entity'])
+        if playlist.get('sg_published_files'):
+            for pf in playlist.get('sg_published_files'):
+                published_files += self.tank.shotgun.find("PublishedFile",
+                                                          [['id', 'is', pf['id']]],
+                                                          ['path',
+                                                           'sg_publish_path',
+                                                           'code',
+                                                           'entity'])
         return published_files
 
     def copy_files_to_playlist_location(self, playlist, published_files, preview):
         output_folder = self.get_ouput_folder(playlist)
-        filepaths = self.get_filepath_list(published_files)
+        filepath_dicts = self.get_filepath_dicts(published_files)
         self.log_info("For playlist %s :" % playlist['code'])
         self.log_info("")
-        self.log_info("Copying the following files into %s:" % output_folder)
+        self.log_info("Creating package in the following folder")
+        self.log_info("%s" % output_folder)
         self.log_info("")
-        for path in filepaths:
-            self.log_info(os.path.basename(path))
+        # for d in filepath_dicts:
+        #     if self.is_video(d['copy_path']) or self.is_nuke_script(d['copy_path']):
+        #         self.log_info(os.path.basename(d['copy_path']))
+        # self.log_info("")
+        for d in filepath_dicts:
+            if self.is_video(d['copy_path']):
+                self.copy_video(d, output_folder, preview)
+            elif self.is_nuke_script(d['copy_path']):
+                nu_path = self.copy_nk(d, output_folder, preview)
+                if nu_path:
+                    self.create_nuke_package_job(nu_path)
 
-        self.log_info("")
-        for path in filepaths:
-            self.copy_file(path, output_folder, preview)
+    def is_video(self, path):
+        return os.path.splitext(path)[1].lower() in video_exts
+
+    def is_nuke_script(self, path):
+        return os.path.splitext(path)[1].lower() in nuke_exts
+
+    def copy_video(self, published_file, output_folder, preview):
+        src = published_file['copy_path']
+        self.all_movs.add(src)
+        dest_folder = os.path.join(output_folder,
+                                   published_file['entity']['name'].upper(),
+                                   "VIDREF")
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+        nu_path = os.path.join(dest_folder, os.path.basename(src))
+        if os.path.exists(nu_path):
+            self.movs_already_existing.add(src)
+        else:
+            if os.path.exists(src):
+                if not preview:
+                    copy(src, nu_path)
+                    self.movs_copied.add(src)
+            else:
+                self.log_exception("MISSING FILE: " + str(src))
+                self.movs_missing.add(src)
+
+    def copy_nk(self, published_file, output_folder, preview):
+        src = published_file['copy_path']
+        self.all_nks.add(src)
+        dest_folder = os.path.join(output_folder,
+                                   published_file['entity']['name'].upper(),
+                                   "NUKE")
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+        nu_path = os.path.join(dest_folder, os.path.basename(src))
+        if os.path.exists(nu_path):
+            self.nks_already_existing.add(src)
+            return nu_path
+
+        else:
+            if os.path.exists(src):
+                if not preview:
+                    copy(src, nu_path)
+                    self.nks_copied.add(src)
+                    return nu_path
+            else:
+                self.log_exception("MISSING FILE: " + str(src))
+                self.nks_missing.add(src)
+
+    def create_nuke_package_job(self, nuke_script):
+        d_path = "/Applications/Thinkbox/Deadline8/Resources/deadlinecommand"
+        nuke_exe = '"C:\\Program Files\\Nuke10.5v4\\Nuke10.5.exe"'
+        current_folder = os.path.dirname(os.path.realpath(__file__))
+        nuke_python_script = os.path.join(current_folder, "nuke_script.py")
+        nuke_python_script = nuke_python_script.replace("/Volumes/FilmShare/", "Y:\\")
+        nuke_python_script = nuke_python_script.replace("/Volumes/projects/", "\\\\\\\\ldn-fs1\\\\projects\\\\")
+        nuke_python_script = nuke_python_script.replace("/", "\\\\")
+        args = '-t %s' % (nuke_script)
+        job_name = "Export Nuke Script for Client: %s" % os.path.basename(nuke_script)
+        cmd = '%s -SubmitCommandLineJob' % d_path
+        cmd += ' -executable %s' % nuke_exe
+        cmd += ' -arguments "%s"' % args
+        cmd += ' -frames 1  -prop LimitGroups=nuker -pool poola -priority 55 -name "%s"' % job_name
+        cmd += ' -prop "EnvironmentKeyValue0=SCRIPT=%s"' % (nuke_script)
+        # print cmd
+        sub.call(cmd, shell=True)
+        self.nks_submitted_for_export.add(nuke_script)
 
     def get_ouput_folder(self, playlist):
         projectPath = self.tank.project_path
@@ -157,20 +260,20 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
         playlistDir = os.path.join(dailiesDir, playlist['code'])
         return playlistDir
 
-    def get_filepath_list(self, published_files):
-        paths = []
+    def get_filepath_dicts(self, published_files):
         for published_file in published_files:
-            if published_file.get('sg_publish_path'):
-                p = self.get_localised_path(published_file['sg_publish_path'])
-                if p:
-                    paths.append(p)
-                # paths.append(published_file['sg_publish_path']['local_path'])
-            elif published_file.get('path'):
+            # if published_file.get('sg_publish_path'):
+            #     p = self.get_localised_path(published_file['sg_publish_path'])
+            #     if p:
+            #         published_file['copy_path'] = p
+            #     # paths.append(published_file['sg_publish_path']['local_path'])
+            # el
+            if published_file.get('path'):
                 p = self.get_localised_path(published_file['path'])
                 if p:
-                    paths.append(p)
+                    published_file['copy_path'] = p
                 # paths.append(published_file['path']['local_path'])
-        return paths
+        return published_files
 
     def get_localised_path(self, path_obj):
         if path_obj.get("local_path"):
@@ -205,23 +308,23 @@ class CopyPlaylistVersionsToFolder(tank.platform.Application):
             files = self.get_sequence_files(source)
             if len(files) == 0:
                 self.log_exception("MISSING FILE: " + str(file))
-                self.missing.add(file)
+                self.movs_missing.add(file)
             dest_folder = os.path.join(dest_folder, self.get_sequence_sub_folder(source))
         if not os.path.exists(dest_folder):
             os.makedirs(dest_folder)
         for file in files:
-            self.all_files.add(file)
+            self.all_movs.add(file)
             nu_path = os.path.join(dest_folder, os.path.basename(file))
             if os.path.exists(nu_path):
-                self.already_existing.add(file)
+                self.movs_already_existing.add(file)
             else:
                 if not preview:
                     if os.path.exists(file):
                         copy(file, nu_path)
-                        self.copied.add(file)
+                        self.movs_copied.add(file)
                     else:
                         self.log_exception("MISSING FILE: " + str(file))
-                        self.missing.add(file)
+                        self.movs_missing.add(file)
 
     def is_sequence(self, path):
         pattern = re.compile(".*%\d+d\..*")
